56a93f369981749260814a2530b688cb2e2da878
Knifedge <imanojbhat@gmail.com>
2023-04-13 17:16:25 +0530

    fix: refactoring changes

--- src/services/apis/login.api.ts
+++ src/services/apis/login.api.ts
@@ -10,23 +10,23 @@ export const loginService = createApi({
   reducerPath: 'login',
   baseQuery,
   tagTypes: ['Login'],
-  endpoints: (build: { mutation: (arg0: { query: ((body: any) => { method: string; url: string; body: any; }) | ((body: any) => { method: string; url: string; body: any; }) | ((body: any) => { method: string; url: string; body: any; }) | ((body: any) => { method: string; url: string; body: any; }) | ((body: any) => { method: string; url: string; }) | ((body: any) => { method: string; url: string; body: any; }) | ((body: any) => { method: string; url: string; body: any; }); }) => any; query: (arg0: { query: () => { method: string; url: string; }; }) => any; }) => ({
+  endpoints: build => ({
     login: build.mutation<any, ILoginRequest>({
-      query: (body: any) => ({
+      query: body => ({
         method: 'POST',
         url: 'auth/login/',
         body,
       }),
     }),
     validateOtp: build.mutation<any, IValidateRequest>({
-      query: (body: any) => ({
+      query: body => ({
         method: 'POST',
         url: 'auth/validate_otp/',
         body,
       }),
     }),
     resendOtp: build.mutation<any, IResendRequest>({
-      query: (body: any) => ({
+      query: body => ({
         method: 'POST',
         url: 'auth/resend_otp/',
         body,
@@ -39,28 +39,28 @@ export const loginService = createApi({
       }),
     }),
     updateUserDetails: build.mutation({
-      query: (body: any) => ({
+      query: body => ({
         method: 'PUT',
         url: 'auth/user/',
         body,
       }),
     }),
     resetPassword: build.mutation({
-      query: (body: any) => ({
+      query: body => ({
         method: 'Post',
         url: 'auth/reset_password/',
         body,
       }),
     }),
     confirmResetPassword: build.mutation({
-      query: (body: any) => ({
+      query: body => ({
         method: 'Post',
         url: 'auth/reset_password/confirm/',
         body,
       }),
     }),
     updatePassword: build.mutation({
-      query: (body: any) => ({
+      query: body => ({
         method: 'POST',
         url: 'auth/password_change/',
         body,
